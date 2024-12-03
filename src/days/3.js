5fe4ddfe2f677530723ede8f561415bdab8da89d
sivaramakrishna.bathula <sivaramakrishna.bathula@gmail.com>
2024-12-03 13:45:46 +0530

    feat(day3): solution for day 3

---
+++ src/days/3.js
@@ -0,0 +1,107 @@
+module.exports.run = async (dayNum, part) => {
+    part = +part;
+    dayNum = +dayNum;
+    if (part === 1) {
+        return part1(dayNum);
+    } else {
+        return part2(dayNum);
+    }
+};
+
+const regex = /mul\((\d+),(\d+)\)/g
+
+async function part1(dayNum) {
+    const data = await loadInput(dayNum);
+    var sequences = extractSequences(data, false);
+    var sum = 0;
+    for(let i=0; i<sequences.length; i++) {
+        var tokens = sequences[i].split(',');
+        var num1 = +(tokens[0].replace('mul(', ''));
+        var num2 = +(tokens[1].replace(')', ''));
+        sum += num1 * num2;
+    }
+    return sum;
+}
+
+function extractSequences(input, withdoanddont) {
+    const result = [];
+    let i = 0; 
+    while (i + 4 < input.length) {
+        if (withdoanddont && input.substring(i, i + 4) === "do()") {
+            result.push('do');
+        } else if (withdoanddont && input.substring(i, i + 7) === `don't()`) {
+            result.push('dont');
+        }
+        if (input.substring(i, i + 4) === "mul(") { 
+            const start = i;
+            const end = input.indexOf(")", start); 
+            if (end !== -1) { 
+                const sequence = input.substring(start, end + 1); 
+                if (isValidSequence(sequence)) { 
+                    result.push(sequence);
+                    i = end + 1; 
+                } else {
+                    i++;
+                }
+            } else {
+                break; 
+            }
+        } else { 
+            i++; 
+        }
+    }
+    return result; 
+}
+
+function isValidSequence(sequence) { 
+    if (sequence.startsWith("mul(") && sequence.endsWith(")")) { 
+        const params = sequence.substring(4, sequence.length - 1).split(","); 
+        if(params.length === 2 && !isNaN(params[0]) && !isNaN(params[1])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+async function part1_(dayNum) {
+    const data = await loadInput(dayNum);
+    var sum = 0;
+    let matches;
+    var count = 0;
+    while ((matches = regex.exec(data)) !== null) {
+        count++;
+        sum += matches[1] * matches[2];
+    }
+    console.log('sequence count', count);
+    return sum;
+}
+
+async function part2(dayNum) {
+    const data = await loadInput(dayNum);
+    var sequences = extractSequences(data, true);
+    var sum = 0;
+    var currentlyDoing = true;
+    for(let i=0; i<sequences.length; i++) {
+        if (sequences[i] === 'do') {
+            currentlyDoing = true;
+        } else if (sequences[i] === 'dont') {
+            currentlyDoing = false;
+        } else {
+            if (!currentlyDoing) {
+                continue;
+            }
+            var tokens = sequences[i].split(',');
+            var num1 = +(tokens[0].replace('mul(', ''));
+            var num2 = +(tokens[1].replace(')', ''));
+            sum += num1 * num2;
+        }
+    }
+    return sum;
+}
+
+const filereader = require('../utils/fileread.js');
+
+async function loadInput(dayNum) {
+    const text = await filereader.readTextFile(dayNum);
+    return text;
+}
